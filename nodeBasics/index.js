4e2e603a81b84190b02549a4206e6c40bd71d588
Narine <narinegasparian1@gmail.com>
2024-01-22 16:56:12 +0000

    Add Chalk library

---
+++ nodeBasics/index.js
@@ -0,0 +1,3 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Kyiv (also spelled Kiev) is the capital and most populous city of Ukraine. It is in north-central Ukraine along the Dnieper River.!'));
